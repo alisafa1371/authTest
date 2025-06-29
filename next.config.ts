e9d4e34d7ee6123dc6c1ded766da153b7d5dbfc7
alisafa1371 <ali_safaeyan@yahoo.com>
2025-06-29 08:21:02 +0330

    final commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: isProd ? "/authTest/" : "",
-  output: "export",
 };
 
 export default nextConfig;
